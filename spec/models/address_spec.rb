f9c72e3e96a935794e255ca10ae51f2752332c04
Andrew Dymond <andy.dymond@gmail.com>
2018-02-07 15:07:43 -0700

    Address spec passing

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -2,31 +2,36 @@ require "rails_helper"
 
 describe Address, type: :model do
   describe "Validations" do
-    it "is valid with a description, street_address, city, state and zip" do
-      address = Address.create(description: "Home", street_address: "123 Fake St", city: "The Angels", state: "CA", zip_code: 01234)
+    it "is valid with a description, street_address, city, state, zip and student" do
+      student = Student.create(name: "Scurvy")
+      address = student.addresses.create(description: "Home", street_address: "123 Fake St", city: "The Angels", state: "CA", zipcode: 01234)
 
       expect(address).to be_valid
     end
     it "is invalid without description" do
-      address = Address.create(street_address: "123 Fake St", city: "The Angels", state: "CA", zip_code: 01234)
+      student = Student.create(name: "Scurvy")
+      address = student.addresses.create(street_address: "123 Fake St", city: "The Angels", state: "CA", zipcode: 01234)
 
       expect(address).to be_invalid
     end
     it "is invalid without street_address" do
-      address = Address.create(description: "Home", city: "The Angels", state: "CA", zip_code: 01234)
+      student = Student.create(name: "Scurvy")
+      address = student.addresses.create(description: "Home", city: "The Angels", state: "CA", zipcode: 01234)
 
       expect(address).to be_invalid
     end
     it "is invalid without city" do
-      address = Address.create(description: "Home", street_address: "123 Fake St", state: "CA", zip_code: 01234)
+      student = Student.create(name: "Scurvy")
+      address = student.addresses.create(description: "Home", street_address: "123 Fake St", state: "CA", zipcode: 01234)
 
       expect(address).to be_invalid
     end
     it "is invalid without zip" do
-      address = Address.create(description: "Home", street_address: "123 Fake St", city: "The Angels", state: "CA")
+      student = Student.create(name: "Scurvy")
+      address = student.addresses.create(description: "Home", street_address: "123 Fake St", city: "The Angels", state: "CA")
 
       expect(address).to be_invalid
-    endg
+    end
   end
 
   describe "Relationships" do
