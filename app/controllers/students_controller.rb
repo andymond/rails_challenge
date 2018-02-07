8580f6dbe608f396239a4dd6c23afe100e998b06
Andrew Dymond <andy.dymond@gmail.com>
2018-02-07 13:47:20 -0700

    User sees student show with name passing

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
 class StudentsController < ApplicationController
   def show
+    @student = Student.find(params[:id])
+  end
 
+  private
+
+  def student_params
+    params.require(:student).permit(:id, :name)
   end
 end
--- spec/students/user_sees_one_student_spec.rb
+++ spec/students/user_sees_one_student_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe "User visits student show page" do
   it "displays student's name" do
-    student = Student.new(name: "Erf")
+    student = Student.create(name: "Erf")
 
     visit 'students/1'
 
