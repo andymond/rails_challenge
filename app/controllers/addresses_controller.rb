b80dd958dec2b41f6675977294ab377b5497efea
Andrew Dymond <andy.dymond@gmail.com>
2018-02-07 15:30:35 -0700

    Add new action to addresses controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,4 +2,10 @@ class AddressesController < ApplicationController
   def index
     @student = Student.find(params[:student_id])
   end
+
+  def new
+    @student = Student.find(params[:student_id])
+    @address = @student.addresses.new
+  end
+
 end
