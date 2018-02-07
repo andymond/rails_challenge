c6412723be6cd71fd61be1011d44135908fb30c2
Andrew Dymond <andy.dymond@gmail.com>
2018-02-07 14:39:06 -0700

    Update user_sees_all_students_spec to check students link to show pages

--- spec/students/user_sees_all_students_spec.rb
+++ spec/students/user_sees_all_students_spec.rb
@@ -10,4 +10,15 @@ describe "User can see list of all students" do
     expect(page).to have_content(student_1.name)
     expect(page).to have_content(student_2.name)
   end
+  it "allows user to click on student to view their show page" do
+    student_1 = Student.create(name: "Georgina")
+    student_2 = Student.create(name: "Squidboy")
+
+    visit "/students"
+    click_on student_1.name
+
+    expect(current_path).to eq("/students/#{student_1.id}")
+    expect(page).to have_content(student_1.name)
+    expect(page).to_not have_content(student_2.name)
+  end
 end
