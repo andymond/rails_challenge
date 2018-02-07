c561abc175f77e2e4fbffda03842efc5e90edb06
Andrew Dymond <andy.dymond@gmail.com>
2018-02-07 15:35:18 -0700

    Add create action and address_params method to addresses controller

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -8,4 +8,18 @@ class AddressesController < ApplicationController
     @address = @student.addresses.new
   end
 
+  def create
+    @student = Student.find(params[:student_id])
+    @address = Address.new(address_params)
+    @address.student_id = @student.id
+    @address.save
+
+    redirect_to student_address_path(@student, @address)
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:description, :street_address, :city, :state, :zipcode)
+  end
 end
