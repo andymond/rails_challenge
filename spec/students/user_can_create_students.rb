b2ed5edd0a1449d072e91673d0d43c1a67403b6b
Andrew Dymond <andy.dymond@gmail.com>
2018-02-07 13:59:01 -0700

    Create spec for user_can_create_student

---
+++ spec/students/user_can_create_students.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+describe "User can create new students" do
+  it "allows user to create student" do
+    visit "/students/new"
+
+    fill_in "student[name]", with: "Holden"
+    click_on "Create Student"
+
+    expect(current_path).to eq("/students")
+    expect(page).to have_content("Holden")
+  end
+end
